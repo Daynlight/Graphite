c515e3ff09d404189b07be8da6f4ad3704f25ebe
Daynlight <danielstodulski000@gmail.com>
2025-11-30 11:19:59 +0100

    Window

--- Graphite/Main.cpp
+++ Graphite/Main.cpp
@@ -1,11 +1,23 @@
-
+#include "Renderer.h"
 
 
 
 
 int main(){
+  CW::Renderer::Renderer renderer;
+  renderer.setWindowTitle("Graphite");
 
 
+
+
+
+  while(!renderer.getWindowData()->should_close){
+    renderer.beginFrame();
+
+
+    renderer.swapBuffer();
+    renderer.windowEvents();
+  };
 
   return 0;
 };
