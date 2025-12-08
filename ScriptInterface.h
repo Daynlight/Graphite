2a36cab50917f0250638d82090821c636a9e1890
Daynlight <danielstodulski000@gmail.com>
2025-12-08 21:58:44 +0100

    Build version v1.0.2

---
+++ ScriptInterface.h
@@ -0,0 +1,34 @@
+// Graphite
+// Copyright 2025 Daynlight
+// Licensed under the Apache License, Version 2.0.
+// See LICENSE file for details.
+
+
+#pragma once
+
+
+#if defined(_WIN32) || defined(_WIN64)
+  #ifdef BUILDING_SCRIPT_DLL
+    #define SCRIPT_API __declspec(dllexport)
+  #else
+    #define SCRIPT_API __declspec(dllimport)
+  #endif
+#else
+  #define SCRIPT_API 
+#endif
+
+
+
+
+
+
+
+
+class ScriptInterface{
+public:
+  virtual ~ScriptInterface() = default;
+  virtual void Init() = 0;
+  virtual void Update() = 0;
+  virtual void Draw() = 0;
+  virtual void Destroy() = 0;
+};
