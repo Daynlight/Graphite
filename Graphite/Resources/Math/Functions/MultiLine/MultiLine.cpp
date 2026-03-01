6acb7c55178b8ce288eb5bdbf7a5f52bb29ab04a
Daynlight <danielstodulski000@gmail.com>
2026-03-01 17:17:43 +0100

    edge connection dumb way

--- Graphite/Resources/Math/Functions/MultiLine/MultiLine.cpp
+++ Graphite/Resources/Math/Functions/MultiLine/MultiLine.cpp
@@ -127,7 +127,6 @@ std::pair<std::array<std::vector<float>, 2>, std::vector<unsigned int>> Graphite
     for(int j = 0; j < 3 * 2; j++)
       mesh.second.emplace_back((i - 1) * 4 + mesh_temp.second[j]);
     
-    // TO change
     for(int j = 0; j < 2; j++)
       mesh.second.emplace_back((i - 1) * 4 + mesh_temp.second[j]);
     mesh.second.emplace_back((i - 1) * 4 - 2);
